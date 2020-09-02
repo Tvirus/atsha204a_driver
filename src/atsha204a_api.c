6c19398521e1e11cd188e085e1b6a0a8c544ffef
L <42616862+Tvirus@users.noreply.github.com>
2020-09-02 20:32:45 +0800

    fix endianness issue in gendig function

--- src/atsha204a_api.c
+++ src/atsha204a_api.c
@@ -798,8 +798,8 @@ int atsha204a_gendig(const struct i2c_client *i2c, u8 zone, u16 slot, const u8 *
     /* 仅唤醒，恢复上一步nonce中的TempKey等相关值 */
     atsha204a_wakeup(i2c);
 
-    param2[0] = (slot & 0xff00) >> 8;
-    param2[1] =  slot & 0xff;
+    param2[0] =  slot & 0xff;
+    param2[1] = (slot & 0xff00) >> 8;
     atsha204a_send_command(i2c, SHA204_GENDIG, zone, param2, data, len);
     msleep(CMD_MAX_TIME_GENDIG);
     if ((0 > atsha204a_read_response(i2c, res, sizeof(res))) || (sizeof(res) != res[0]))
