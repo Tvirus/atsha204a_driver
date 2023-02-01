c8474381b7846f3e0312d2e1e52ae2b971683a74
L <tvirus347@gmail.com>
2023-02-01 17:00:30 +0800

    fix warning

--- src/atsha204a_module.c
+++ src/atsha204a_module.c
@@ -370,7 +370,7 @@ static int atsha204a_probe(struct i2c_client *client, const struct i2c_device_id
     sha204_sysdata = kzalloc(sizeof(atsha204a_sysdata_t), GFP_KERNEL);
     if (IS_ERR_OR_NULL(sha204_sysdata))
     {
-        ERROR("cannot allocate memory, size: %lu !", sizeof(atsha204a_sysdata_t));
+        ERROR("cannot allocate memory, size: %zu !", sizeof(atsha204a_sysdata_t));
         return -ENOMEM;
     }
     sha204_sysdata->client = client;
